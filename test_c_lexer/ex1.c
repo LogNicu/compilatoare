078a2922574e377301db570a1cce324ab98e3fea
nicu <log.nicu@gmail.com>
2023-11-04 19:53:39 +0200

    Some more operators added to the parser

--- test_c_lexer/ex1.c
+++ test_c_lexer/ex1.c
@@ -5,5 +5,13 @@ int main() {
   for(int i = 0; i < 10 && i > 0; i++) {
    print(i);
   }
+  for(int i = 0; i <= 10  || i >= 0; --i, i++) {
+    do{
+      if(x == 20) {
+        printf("this code is not supposed to make sense and won't compile");
+        char_literal = 'A';
+      }
+    }while(x++);
+  }
   //this is a line comment;
 }
